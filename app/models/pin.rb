15fc75a449eaba8881144be251220831043a2bf4
Joshua Spears <josh@woofound.com>
2014-01-05 21:12:44 -0500

    Add name to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,6 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100X100"}
+	validates :description, presence: true
+	validates :image, presence: true
 end
